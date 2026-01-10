1ea73b7bc2ac45a5c53a9e12e95447bbdad649a3
jkk290 <jared@kuromoto.dev>
2026-01-09 16:34:08 -1000

    Complete chapter 8 project 6 and start 7

---
+++ ch8/5x5.c
@@ -0,0 +1,23 @@
+/* Reads a 5x5 array of ints and prints the row sums and column sums */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a[5][5];
+	int row;
+
+	for (row = 0; row < 5; row++) {
+		printf("Enter row i: ");
+		scanf("%d %d %d %d %d", &a[row][0],&a[row][1], &a[row][2], &a[row][3], &a[row][4]);
+	}
+
+	int row_totals, col_totals;
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+
+
+
+	return 0;
+}
--- ch8/biff.c
+++ ch8/biff.c
@@ -2,7 +2,51 @@
 /* A > 4, B > 8, E > 3, I > 1, O > 0, S > 5.  Append 10 !'s */
 
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
+	int i = 0;
+	char ch;
+	char chs[64];
+	printf("Enter message: ");
+	ch = getchar();
 
+	while (ch != '\n') {
+		chs[i] = toupper(ch);
+		i++;
+		ch = getchar();
+	}
+
+	printf("In B1FF-speak: ");
+
+	for (int j = 0; j <= i; j++) {
+		switch (chs[j]) {
+				case 'A':
+					printf("4");
+					break;
+				case 'B':
+					printf("8");
+					break;
+				case 'E':
+					printf("3");
+					break;
+				case 'I':
+					printf("1");
+					break;
+				case 'O':
+					printf("0");
+					break;
+				case 'S':
+					printf("5");
+					break;
+				default:
+					printf("%c", chs[j]);
+					break;
+		}
+	}
+
+	printf("!!!!!!!!!!\n");
+
+	return 0;
+}
